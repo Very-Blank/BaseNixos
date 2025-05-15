39f4a855884112e9e859fff6ef5b9d74d19c5872
very-blank <aapeli.saarelainen.76@gmail.com>
2025-05-15 12:17:50 +0300

    fix: mako

--- homeModules/mako.nix
+++ homeModules/mako.nix
@@ -5,7 +5,7 @@
 
     settings = {
       default-timeout = 1500;
-      backgroundColor = "#7c818c";
+      background-color = "#7c818c";
       font = "0xProtoNerdFont-Regular 10";
       border-radius = 10;
       border-size = 0;
