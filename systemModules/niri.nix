7aec299342901a86f6aeea14bed7e35f6ae5d45b
very-blank <aapeli.saarelainen.76@gmail.com>
2025-06-02 10:07:03 +0300

    fix: Unstable missing things

--- systemModules/niri.nix
+++ systemModules/niri.nix
@@ -1,7 +1,7 @@
 {inputs, pkgs, ...}:
 {
   nixpkgs.overlays = [inputs.niri.overlays.niri];
-  programs.niri.package = pkgs.niri-unstable;
+  programs.niri.package = pkgs.niri-stable;
 
   programs.niri = {
     enable = true;
